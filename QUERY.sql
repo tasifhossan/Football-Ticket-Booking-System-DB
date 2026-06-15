e17ed7c0a40ef1cc8d5501321ca4ba51a0d6364b
Tasif <tasifhossan01@gmail.com>
2026-06-15 22:09:39 +0600

    Query 1 & 2

--- QUERY.sql
+++ QUERY.sql
@@ -88,3 +88,26 @@ INSERT INTO Bookings (booking_id, user_id, match_id, seat_number, payment_status
 (503, 2, 101, 'A-13', 'Confirmed', 150.00),
 (504, 2, 101, NULL, NULL, 150.00),
 (505, 3, 102, 'C-20', 'Pending', 120.00);
+
+
+
+-- Query 01
+
+SELECT
+    match_id,
+    fixture,
+    base_ticket_price
+FROM Matches
+WHERE tournament_category = 'Champions League'
+  AND match_status = 'Available';
+
+  -- Query 2
+
+  SELECT
+    user_id,
+    full_name,
+    email
+FROM Users
+WHERE full_name ILIKE 'Tanvir%'
+   OR full_name ILIKE '%Haque%';
+ 
